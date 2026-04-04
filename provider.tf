575d66ec3b539eccc2b6185aa9d9f69a6ea3357d
Azure DevOps Pipeline <pipeline@devops.com>
2026-04-04 23:53:31 +1100

    fix: allow resource group deletion with nested resources on destroy

--- provider.tf
+++ provider.tf
@@ -7,7 +7,10 @@ terraform {
   }
 }
 
-# Configure the Azure Provider
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
